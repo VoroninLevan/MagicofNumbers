f84db613b0ca5e1ab8eff9dfc5c268fd9a899a77
Levan Voronin <voroninlevan@gmail.com>
2017-11-11 02:21:07 +0300

    New Complex activity (under dev)

---
+++ app/src/main/java/comvoroninlevan/httpsgithub/magicofnumbers/ComplexActivity.java
@@ -0,0 +1,260 @@
+package comvoroninlevan.httpsgithub.magicofnumbers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+/**
+ * Created by Levan on 03.11.2017.
+ */
+
+public class ComplexActivity extends AppCompatActivity {
+
+    private Random random;
+    private ImageView leftSign, rightSign, inBracketSign;
+    private RelativeLayout leftBlock, rightBlock;
+    private ImageView numOneOne, numOneTwo, numTwoOne, numTwoTwo, numThreeOne, numThreeTwo,
+            numFourOne, numFourTwo;
+    private int level = 10;
+    private int numberLeft, resultBracket, numberRight;
+    private boolean additionLeft, additionBracket, additionRight;
+    private String leftFlag = "left";
+    private String bracketFlag = "bracket";
+    private String rightFlag = "right";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complex);
+        hide();
+
+        random = new Random();
+
+        leftBlock = (RelativeLayout)findViewById(R.id.leftBlock);
+        rightBlock = (RelativeLayout)findViewById(R.id.rightBlock);
+
+        inBracketSign = (ImageView)findViewById(R.id.inBracketSign);
+        leftSign = (ImageView)findViewById(R.id.leftSign);
+        rightSign = (ImageView)findViewById(R.id.rightSign);
+
+        numOneOne = (ImageView)findViewById(R.id.numOneOne);
+        numOneTwo = (ImageView)findViewById(R.id.numOneTwo);
+        numTwoOne = (ImageView)findViewById(R.id.numTwoOne);
+        numTwoTwo = (ImageView)findViewById(R.id.numTwoTwo);
+        numThreeOne = (ImageView)findViewById(R.id.numThreeOne);
+        numThreeTwo = (ImageView)findViewById(R.id.numThreeTwo);
+        numFourOne = (ImageView)findViewById(R.id.numFourOne);
+        numFourTwo = (ImageView)findViewById(R.id.numFourTwo);
+
+        generateQuiz();
+    }
+
+    private void hide() {
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    private void generateQuiz(){
+        if(exerciseLayout() == 0){
+            hideRightPart(true);
+            setSignsWithLeft(true);
+            setNumbersQuizWithLeft(true);
+        }else{
+            hideRightPart(false);
+            setSignsWithLeft(false);
+            setNumbersQuizWithLeft(false);
+        }
+    }
+
+    private int exerciseLayout(){
+        return(random.nextInt(2));
+    }
+
+    private void hideRightPart(boolean hide){
+        if(hide){
+            rightBlock.setVisibility(View.GONE);
+            rightSign.setVisibility(View.GONE);
+        }else{
+            leftBlock.setVisibility(View.GONE);
+            leftSign.setVisibility(View.GONE);
+        }
+    }
+
+    private void setSignsWithLeft(boolean leftPart){
+        int bracketSign = returnDrawableSign();
+        inBracketSign.setImageResource(bracketSign);
+        if(bracketSign == R.drawable.addition){
+            additionBracket = true;
+        }else{
+            additionBracket = false;
+        }
+        if(leftPart){
+            int sign = returnDrawableSign();
+            leftSign.setImageResource(sign);
+            if(sign == R.drawable.addition){
+                additionLeft = true;
+            }else{
+                additionLeft = false;
+            }
+        }else{
+            int sign = returnDrawableSign();
+            rightSign.setImageResource(sign);
+            if(sign == R.drawable.addition){
+                additionRight = true;
+            }else{
+                additionRight = false;
+            }
+        }
+    }
+
+    private int returnDrawableSign(){
+        if(random.nextInt(2) == 0){
+            return R.drawable.addition;
+        }else{
+            return R.drawable.subtraction;
+        }
+    }
+
+    private void setNumbersQuizWithLeft(boolean leftPart){
+        setNumbersInBracket();
+        if(leftPart){
+            int[] nums = getRandomNumbers();
+            int leftNumber = transFromFromArrayToInt(nums);
+            if(!additionLeft){
+                if(leftNumber < resultBracket){
+                    while (leftNumber < resultBracket){
+                        nums = getRandomNumbers();
+                        leftNumber = transFromFromArrayToInt(nums);
+                    }
+                }
+            }
+            if(nums[1] == -1){
+                numOneTwo.setImageResource(setDrawableNum(nums[0]));
+            }else{
+                numOneOne.setImageResource(setDrawableNum(nums[0]));
+                numOneTwo.setImageResource(setDrawableNum(nums[1]));
+            }
+        }else{
+            int[] nums = getRandomNumbers();
+            int rightNumber = transFromFromArrayToInt(nums);
+            if(!additionRight){
+                if(rightNumber > resultBracket){
+                    while (rightNumber > resultBracket){
+                        nums = getRandomNumbers();
+                        rightNumber = transFromFromArrayToInt(nums);
+                    }
+                }
+            }
+            if(nums[1] == -1){
+                numFourTwo.setImageResource(setDrawableNum(nums[0]));
+            }else{
+                numFourOne.setImageResource(setDrawableNum(nums[0]));
+                numFourTwo.setImageResource(setDrawableNum(nums[1]));
+            }
+        }
+    }
+
+    private void setNumbersInBracket(){
+        int[] numsFirst = getRandomNumbers();
+        int numberOneBracket = transFromFromArrayToInt(numsFirst);
+        int[] numsSecond = getRandomNumbers();
+        int numberTwoBracket = transFromFromArrayToInt(numsSecond);
+        if(!additionBracket){
+            if(numberOneBracket < numberTwoBracket){
+                while (numberOneBracket < numberTwoBracket){
+                    numsSecond = getRandomNumbers();
+                    numberTwoBracket = transFromFromArrayToInt(numsSecond);
+                }
+            }
+        }
+
+        resultBracket = getResultBracket(numberOneBracket, numberTwoBracket);
+        if(numsFirst[1] == -1){
+            numTwoTwo.setImageResource(setDrawableNum(numsFirst[0]));
+        }else{
+            numTwoOne.setImageResource(setDrawableNum(numsFirst[0]));
+            numTwoTwo.setImageResource(setDrawableNum(numsFirst[1]));
+        }
+
+        if(numsSecond[1] == -1){
+            numThreeTwo.setImageResource(setDrawableNum(numsSecond[0]));
+        }else{
+            numThreeOne.setImageResource(setDrawableNum(numsSecond[0]));
+            numThreeTwo.setImageResource(setDrawableNum(numsSecond[1]));
+        }
+    }
+
+    private int getResultBracket(int numOne, int numTwo){
+        if(additionBracket){
+            return numOne + numTwo;
+        }else{
+            return numOne - numTwo;
+        }
+    }
+
+    private int[] getRandomNumbers(){
+        int[] nums = new int[2];
+        String stringNum = Integer.toString(random.nextInt(level));
+        String firstPart = stringNum.substring(0,1);
+        int firstNumPart;
+        int secondNumPart = -1;
+        firstNumPart = Integer.parseInt(firstPart);
+        nums[0] = firstNumPart;
+        nums[1] = secondNumPart;
+        if(stringNum.length() > 1){
+            String secondPart = stringNum.substring(1);
+            secondNumPart = Integer.parseInt(secondPart);
+            nums[1] = secondNumPart;
+        }
+        return nums;
+    }
+
+    private int setDrawableNum(int x){
+        //Set image res based on x
+        switch(x){
+            case 0:
+                return R.drawable.zero;
+            case 1:
+                return R.drawable.one;
+            case 2:
+                return R.drawable.two;
+            case 3:
+                return R.drawable.three;
+            case 4:
+                return R.drawable.four;
+            case 5:
+                return R.drawable.five;
+            case 6:
+                return R.drawable.six;
+            case 7:
+                return R.drawable.seven;
+            case 8:
+                return R.drawable.eight;
+            default:
+                return R.drawable.nine;
+        }
+    }
+
+    private int transFromFromArrayToInt(int[] array){
+        String firstNum = String.valueOf(array[0]);
+        String secondNum = "";
+        if(!(array[1] == -1)){
+            secondNum = String.valueOf(array[1]);
+        }
+        String full = firstNum + secondNum;
+        return(Integer.parseInt(full));
+    }
+}
